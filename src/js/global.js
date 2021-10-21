b30487da718592bf588e43fe22cbda9eb30c23cf
gtasa3box <gtasa3box@gmail.com>
2021-10-21 17:17:53 +0300

    dfs

--- src/js/global.js
+++ src/js/global.js
@@ -1,4 +1,27 @@
+//burger
+const scrolllistener = () => {
+    window.scrollTo(0, 0);
+};
+
 $('.burger').click(function() {
     $(this).toggleClass('active');
     $('.nav').toggleClass('active');
+    if (
+        $('.nav').hasClass('active')
+        ) {
+        window.addEventListener(
+            'scroll', scrolllistener
+        )
+    }
+    else {
+        window.removeEventListener(
+            'scroll',scrolllistener
+        )
+    }
+});
+
+//drop-nav
+$('.tringle-drop').on('click', function(){
+    $(this).toggleClass('active') 
+    $($(this).data('target')).stop(true, false).slideToggle()  
 });
