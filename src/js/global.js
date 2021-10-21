069d8c9dfb24fd08f132038d3249809b02fcf8ae
gtasa3box <gtasa3box@gmail.com>
2021-10-21 20:12:24 +0300

    dfs

--- src/js/global.js
+++ src/js/global.js
@@ -25,3 +25,43 @@ $('.tringle-drop').on('click', function(){
     $(this).toggleClass('active') 
     $($(this).data('target')).stop(true, false).slideToggle()  
 });
+
+//slider-works
+$('.slider-gallery-works').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1619,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1099,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+});
